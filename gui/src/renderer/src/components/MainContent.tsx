249778c4ee1d7143029e72adc4ba4ecd7c0ecb96
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2025-04-24 14:41:49 -0500

    remove gsap

--- gui/src/renderer/src/components/MainContent.tsx
+++ gui/src/renderer/src/components/MainContent.tsx
@@ -6,11 +6,6 @@ import { Content } from 'antd/es/layout/layout'
 import PendingContent from './PendingContent'
 import NewEncrypt from './NewEncrypt'
 import LoadCard from './LoadCard'
-import Flip from 'gsap/Flip'
-import gsap from 'gsap'
-
-// Register GSAP plugins
-gsap.registerPlugin(Flip)
 
 function MainContent() {
   const [selectedItem, setSelectedItem] = useState<string>()
@@ -34,53 +29,6 @@ function MainContent() {
       setSelectedItem(menuItem)
     }
   }, [menuItem])
-
-  useEffect(() => {
-    const content = contentRef.current
-    if (!content) return
-
-    const cardsSelector = '.ant-card, .skeleton-card'
-    let previousWidth: number | undefined
-
-    /**
-     * @description `[ESP]` Este cambio mas que nada se realizo porque GSAP hacia un bucle infinito con el transorm y transform3d porque el Content pasaba de tener overflow a no tenerlo al aplicar a todas las cards la propiedad absolute 🤡.
-     * @description `[ENG]` This change was made because GSAP caused an infinite loop with the transform and transform3d because the Content changed from having overflow to not having it when applying the absolute property to all cards.
-     */
-    const handleResize = (entries: ResizeObserverEntry[]) => {
-      const { contentRect } = entries[0]
-      const currentWidth = contentRect.width ?? 0
-      const difference = Math.abs(currentWidth - (previousWidth ?? 0))
-
-      // Only animate if the difference is significant
-      if (difference > 5) {
-        const shouldCenter = content.offsetWidth < 770
-        content.classList.toggle('justify-center', shouldCenter)
-
-        // Animate the cards
-        const state = Flip.getState(cardsSelector)
-        Flip.from(state, {
-          ease: 'power1.inOut',
-          absolute: true,
-          duration: 0.5,
-          onComplete: () => {
-            gsap.set(cardsSelector, { clearProps: 'all' })
-          }
-        })
-
-        previousWidth = currentWidth
-      }
-
-      // Save the current width for the next resize event
-      if (!previousWidth && currentWidth > 0) {
-        previousWidth = currentWidth
-      }
-    }
-
-    const observer = new ResizeObserver(handleResize)
-    observer.observe(content)
-
-    return () => observer.disconnect()
-  }, [])
 
   return (
     <Layout className="h-screen">
