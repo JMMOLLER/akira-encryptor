42896489a769843e7fea934eed0fcd6d79c67c0e
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2025-06-12 13:43:26 -0500

    fix(core): remove .js extension

--- core/workers/encryptor.worker.ts
+++ core/workers/encryptor.worker.ts
@@ -1,5 +1,5 @@
-import decryptFile from "../crypto/decryptFile.js";
-import encryptFile from "../crypto/encryptFile.js";
+import decryptFile from "../crypto/decryptFile";
+import encryptFile from "../crypto/encryptFile";
 
 export default async function run(params: WorkerTask) {
   const { taskType, filePath, tempPath, SECRET_KEY, blockSize } = params;
