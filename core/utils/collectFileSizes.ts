b2e3938a78452d53a10c723fb9a83cdc6c02dde2
Jorge Luis Moreno Moller <jlmm.jlmm76@gmail.com>
2025-06-27 17:37:29 -0500

    refactor(core): prevent unnecessary instance call

--- core/utils/collectFileSizes.ts
+++ core/utils/collectFileSizes.ts
@@ -1,9 +1,8 @@
 import { FileSystem } from "../libs/FileSystem";
 
-const STORAGE = FileSystem.getInstance();
-
 export default function collectFileSizes(dirPath: string): number {
   let totalSize = 0;
+  const STORAGE = FileSystem.getInstance();
   const entries = STORAGE.readDir(dirPath);
 
   for (const entry of entries) {
